27e9a1f32bc2cab8cae0a1d6c97922bb1cd73e47
Shchipanov <chips_80@mail.ru>
2022-06-08 09:11:42 +0400

    устряняет замечания getRandomInt, переименованияе lemited

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно.
 // источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+function getRandomInt(min, max) {
+  return Math.random() * (max - min) + min;
 }
 getRandomInt();
 
@@ -10,8 +10,9 @@ getRandomInt();
 // Функция для проверки максимальной длины строки
 // источник https://rche.ru/434_ogranichit-maks-dlinu-stroki-v-pole-input-primer-na-javascript.html
 
-function limiter(fild, size) {
+function getMaximumStringlength(fild, size) {
   if (fild.value.length > size) {
-    fild.value = fild.value.substring(0, size);}
+    fild.value = fild.value.substring(0, size);
+  }
 }
-limiter();
+getMaximumStringlength();
