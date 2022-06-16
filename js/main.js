12891e98aaef6b857b200584fbd0e9595f31023d
Shchipanov <chips_80@mail.ru>
2022-06-16 19:46:14 +0400

    устранение замечаний, ругается линтер на id,

--- js/main.js
+++ js/main.js
@@ -1,58 +1,4 @@
-// Функция, возвращающая случайное целое число из переданного диапазона включительно.
-// источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-
-// const { name } = require("browser-sync");
-
-// function getRandomInt(min, max) {
-//   return Math.random() * (max - min) + min;
-// }
-// getRandomInt();
-
-// Функция от Кекса getRandomPositiveInteger, возвращающая случайное целое число из переданного диапазона включительно
-
-// function getRandomPositiveInteger (a, b) {
-//   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-//   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-//   const result = Math.random() * (upper - lower + 1) + lower;
-//   return Math.floor(result);
-// }
-
-// Функция для проверки максимальной длины строки
-// источник https://rche.ru/434_ogranichit-maks-dlinu-stroki-v-pole-input-primer-na-javascript.html
-
-// function getMaximumStringlength(fild, size) {
-//   if (fild.value.length > size) {
-//     fild.value = fild.value.substring(0, size);
-//   }
-// }
-// getMaximumStringlength();
-
-// Функция от Кекса checkStringLength для проверки максимальной длины строки
-
-// function checkStringLength (string, length) {
-//   return string.length <= length;
-// }
-// checkStringLength();
-
-// Создания массива из 25 сгенерированных объектов
-
-// const makePhoto = () => ({
-//   id,
-// });
-
-// const generatePhotos = (count) => {
-//   const photos = [];
-//   for (let i = 1; i <= count; i++) {
-//     photos.push(makePhoto(i));
-//   }
-
-//   return photos;
-// }
-
-// const MAX_PHOTO_COUNT = 25;
-// const photos = generatePhotos(MAX_PHOTO_COUNT);
-
-const MakePhotoId = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+// Создание массива из 25 сгенерированных объектов
 
 const URL_INDEX = ['photos/1.jpg', 'photos/2.jpg', 'photos/3.jpg', 'photos/4.jpg', 'photos/5.jpg', 'photos/6.jpg', 'photos/7.jpg', 'photos/8.jpg', 'photos/9.jpg', 'photos/10.jpg', 'photos/11.jpg', 'photos/12.jpg', 'photos/13.jpg', 'photos/14.jpg', 'photos/15.jpg', 'photos/16.jpg', 'photos/17.jpg', 'photos/18.jpg', 'photos/19.jpg', 'photos/20.jpg', 'photos/21.jpg', 'photos/22.jpg', 'photos/23.jpg', 'photos/24.jpg', 'photos/25.jpg'];
 
@@ -62,6 +8,19 @@ const MESSAGE = ['Я поскользнулся на банановой кожу
 
 const NAME = ['Евпатий', 'Хон-Гильдон', 'Мишенька', 'Андрюша', 'Алексей'];
 
+const MIN_COUNT = 1;
+
+const MAX_COUNT = 25;
+
+const MIN_IMG_NUMBER = 1;
+
+const MAX_IMG_NUMBER = 6;
+
+const MIN_LIKES_COUNT = 15;
+
+const MAX_LIKES_COUNT = 200;
+
+// функция ввывода случайных чисел
 function getRandomPositiveInteger (a, b) {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
@@ -71,47 +30,47 @@ function getRandomPositiveInteger (a, b) {
 
 const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
 
-
+// функция проверки длины строки
 const checkStringLength = (string, length) => string.length <= length;
 
-// const getPhotoDecriotion = () => {
-//   const getRandomDescrIndex = getRandomPositiveInteger(0, MakePhotoId.length - 1);
-//   const getRandomMessageIndex = getRandomPositiveInteger(0, MESSAGE - 1);
-//   const getRandomNameIndex = getRandomPositiveInteger(0, NAME - 1);
+// итоговая функция вывода значений
+const generatePhotos = () => {
 
-//   const photoComments = [ {
-//     id: getRandomPositiveInteger(1,25),
-//     avatar: `img/avatar-${getRandomPositiveInteger(1,6)}.svg`,
-//     message: getRandomPositiveInteger(MESSAGE),
-//     name: getRandomPositiveInteger(NAME),
-//   }
-//   ];
-// }
+  // функция поиска Id
+  const makePhotoId = () => ({
+    id,
+  });
 
-const photoDescription = () => {
-  const getRandomDescrIndex = getRandomPositiveInteger(0, MakePhotoId.length - 1);
-  const getRandomMessageIndex = getRandomPositiveInteger(0, MESSAGE - 1);
-  const getRandomNameIndex = getRandomPositiveInteger(0, NAME - 1);
+  const generatePhotosId = (count) => {
+    const photosId = [];
+    for (let i = 1; i <= count; i++) {
+      photosId.push(makePhotoId(i));
+    }
+    return photosId;
+  };
 
-  const photoComments = [ {
-    id: getRandomPositiveInteger(1,25),
-    avatar: `img/avatar-${getRandomPositiveInteger(1,6)}.svg`,
-    message: getRandomPositiveInteger(MESSAGE),
-    name: getRandomPositiveInteger(NAME),
-  }
-  ];
+  const PHOTOS_ID = generatePhotosId(MAX_COUNT);
 
-  getRandomDescrIndex();
+  const getRandomMessageIndex = getRandomArrayElement(0, MESSAGE - 1);
+  const getRandomNameIndex = getRandomArrayElement(0, NAME - 1);
+
+  const photoComments = {
+    id: getRandomPositiveInteger(MIN_COUNT, MAX_COUNT),
+    avatar: `img/avatar-${getRandomArrayElement(MIN_IMG_NUMBER, MAX_IMG_NUMBER)}.svg`,
+    message: getRandomArrayElement(MESSAGE),
+    name: getRandomArrayElement(NAME),
+  };
+
   getRandomMessageIndex();
   getRandomNameIndex();
   return{
-    id: getRandomArrayElement(MakePhotoId),
+    id: PHOTOS_ID(),
     url: getRandomArrayElement(URL_INDEX),
     description: getRandomArrayElement(DESCRIPTION_INDEX),
-    likes: checkStringLength(15, 200),
-    comments: photoComments,
+    likes: checkStringLength(MIN_LIKES_COUNT, MAX_LIKES_COUNT),
+    comments: photoComments(),
   };
 };
 
-photoDescription();
+generatePhotos();
 
